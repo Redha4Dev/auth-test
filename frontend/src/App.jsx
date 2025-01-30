756f7ab7e422eb096ac80e9d5b41ea453e01a9f5
Redha4Dev <devredha@gmail.com>
2025-01-30 15:33:29 +0100

    Add new UI componant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Users, setUsers] = useState([]);
 
+  useEffect(() => {
+    axios
+    .get("https://localhost:5000/users")
+    .then((response) => setUsers(response.data))
+    .catch((err) => console.log(err))
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Users List :</h1>
+      <ul>
+        {Users.map((user) => <li key={user.id}>{user.name} - {user.age} Years Old</li>)}
+      </ul>
+    </div>
   )
 }
 
