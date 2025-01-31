cd6624f7f89017245933dcf600f591aa63689389
Ilyes Mekhalfa. <ilyes.mekalfa@gmail.com>
2025-01-31 22:29:37 +0100

    test implmentation

---
+++ backend/testapp.js
@@ -0,0 +1,38 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const users = [{
+    name: 'John Doe',
+    age: 25,
+    id: 1
+},{
+    name: 'Jane Doe',
+    age: 24,
+    id: 2
+},
+{
+    name: 'John Smith',
+    age: 30,
+    id: 3
+},{
+    name: 'Jane Smith',
+    age: 29,
+    id: 4
+}];
+
+app.get('/',(req,res) =>{
+    res.status(200).json({
+        message : 'test',
+        users
+    })
+});
+
+const port = 8000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
