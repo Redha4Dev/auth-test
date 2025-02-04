785afabe0840b32df6d3edb0fe3e5fabd6b63fbc
Redha4Dev <devredha@gmail.com>
2025-02-04 08:21:42 +0100

    modified:   frontend/src/App.jsx 	modified:   frontend/src/index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,12 +24,14 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Sending Data :</h1>
-        <input type="text" name="name" value={dataF.name} onChange={handleChange} />
-        <input type="email" name="email" value={dataF.email} onChange={handleChange} />
-        <button type="submit">Submit</button>
+    <div className="bg-white h-screen p-20 w-screen">
+      <form onSubmit={handleSubmit} className="bg-[#EFEFEF] w-fit mx-auto flex flex-col p-6 gap-4 border-[1px] border-black rounded-2xl shadow-2xl ">
+        <h1 className="text-black font-medium">Sending Data :</h1>
+        <label className="font-xl text-black font-medium">Name :</label>
+        <input className="bg-white border-black border-[1px] rounded-xl text-xl outline-none text-black px-4" type="text" name="name" value={dataF.name} onChange={handleChange} />
+        <label className="font-xl text-black font-medium">Email :</label>
+        <input className="bg-white border-black border-[1px] rounded-xl text-xl outline-none text-black px-4" type="email" name="email" value={dataF.email} onChange={handleChange} />
+        <button className="ouline-none" type="submit">Submit</button>
       </form>
 
       {response && (
