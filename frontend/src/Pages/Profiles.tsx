1770b4310168e7098f6f7b326fd80dd121ff6f1e
Redha4Dev <devredha@gmail.com>
2025-02-08 10:52:15 +0100

    modified:   backend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/Pages/Profiles.tsx 	modified:   frontend/src/utils/ProtectedRouters.jsx

--- frontend/src/Pages/Profiles.tsx
+++ frontend/src/Pages/Profiles.tsx
@@ -1,9 +1,29 @@
+import axios from 'axios'
 import React from 'react'
 
-function Porfiles() {
+function Profiles() {
+  const [kids, setKids] = React.useState([]);
+  //fetch data using axios from the backend
+  React.useEffect(() => {
+    const fetchKids = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/Admin/manage-kids");
+        console.log(response.data);
+        setKids(response.data);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    }
+    fetchKids();
+  }, []);
+  console.log(kids);
   return (
-    <div>Porfiles</div>
+    <div>
+      <ul>
+        
+      </ul>
+    </div>
   )
 }
 
-export default Porfiles
+export default Profiles
