8f4400ce372653eed215a5ee10232c8fe442830f
Zakaria Medjdoub <mz.medjdoub@esi-sba.dz>
2025-02-18 21:06:34 +0100

    Creation of the mobile app
    Added the welcome page

---
+++ mobile/kidergarten/lib/pages/welcome_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:kidergarten/components/myButton.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 79, 41, 159),
+          Color.fromARGB(255, 2 + 11 * 16, 10 * 16 + 2, 14 * 16 + 3)
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150,
+              ),
+              Image.asset(
+                "assets/family_picture.png",
+                scale: 1.2,
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Mybutton(
+                title: 'I am a Teacher',
+              ),
+              Mybutton(
+                title: 'I am a Parent',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
