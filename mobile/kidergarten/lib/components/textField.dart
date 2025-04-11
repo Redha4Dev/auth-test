1ff5a2535ef4c5d73924b42b175ea923bd30d236
Zakaria Medjdoub <mz.medjdoub@esi-sba.dz>
2025-04-11 17:11:35 +0100

    cleaned up the signup page

--- mobile/kidergarten/lib/components/textField.dart
+++ mobile/kidergarten/lib/components/textField.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class myTextField extends StatelessWidget {
+class myTextField extends StatefulWidget {
   final String labelText;
   final IconData icon;
   final TextEditingController? controller;
@@ -8,11 +8,24 @@ class myTextField extends StatelessWidget {
 
   const myTextField({
     Key? key,
-    required this.labelText, // Label text as input
-    required this.icon, // Icon as input
+    required this.labelText,
+    required this.icon,
     this.controller,
-    this.isPassword = false, // To handle password fields
+    this.isPassword = false,
   }) : super(key: key);
+
+  @override
+  State<myTextField> createState() => _myTextFieldState();
+}
+
+class _myTextFieldState extends State<myTextField> {
+  late bool _obscure;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscure = widget.isPassword;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,35 +36,45 @@ class myTextField extends StatelessWidget {
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color: Colors.black
-                  .withOpacity(0.2), // Shadow color with transparency
-              spreadRadius: 1, // How much the shadow spreads
-              blurRadius: 2, // Softness of the shadow
-              offset: Offset(0, 6), // Position: (X, Y)
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: Offset(0, 6),
             ),
           ],
-          color: Color(0xFFAF92D8), // Light purple background
-          borderRadius: BorderRadius.circular(30), // Rounded corners
+          color: Color(0xFFAF92D8),
+          borderRadius: BorderRadius.circular(30),
         ),
         child: Row(
           children: [
-            Icon(icon, color: Colors.white), // Dynamic icon
-            SizedBox(width: 10), // Space between icon and text
-
-            SizedBox(width: 10), // Space before TextField
+            Icon(widget.icon, color: Colors.white),
+            SizedBox(width: 10),
             Expanded(
-              child: TextField(
-                controller: controller,
-                obscureText: isPassword, // For password fields
-                decoration: InputDecoration(
-                  border: InputBorder.none, // No underline
-                  hintText: labelText,
-                  hintStyle: TextStyle(color: Colors.white70),
-                ),
+              child: TextFormField(
+                controller: widget.controller,
+                obscureText: _obscure,
                 style: TextStyle(color: Colors.white),
-                cursorColor: Colors.white, // Cursor color
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  labelText: widget.labelText,
+                  labelStyle: TextStyle(color: Colors.white70),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  border: InputBorder.none,
+                ),
               ),
             ),
+            if (widget.isPassword)
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscure = !_obscure;
+                  });
+                },
+                child: Icon(
+                  _obscure ? Icons.visibility_off : Icons.visibility,
+                  color: Colors.white,
+                ),
+              ),
           ],
         ),
       ),
--- mobile/kidergarten/lib/pages/signup_page.dart
+++ mobile/kidergarten/lib/pages/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kidergarten/components/myButton.dart';
 import 'package:kidergarten/components/textField.dart';
 import 'package:kidergarten/pages/login_page.dart';
@@ -22,49 +23,39 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController phoneController = TextEditingController();
 
   bool isLoading = false;
-
-  // Gender and Role values
   String gender = "Male";
   String role = "teacher";
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    final ApiService apiService = ApiService();
+    final apiService = ApiService();
+    final mediaQuery = MediaQuery.of(context);
+    final screenHeight = mediaQuery.size.height;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        width: screenWidth,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 79, 41, 159),
-              Color.fromARGB(255, 34, 162, 227)
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            Container(
-              height: screenHeight * .7,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-              ),
+    return GestureDetector(
+      onTap: () =>
+          FocusScope.of(context).unfocus(), // dismiss keyboard on tap outside
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 79, 41, 159),
+                Color.fromARGB(255, 34, 162, 227)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            ListView(
-              children: [
-                SizedBox(height: screenHeight * .05),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          child: SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
                   children: [
                     Image.asset(
                       widget.isParent
@@ -72,203 +63,173 @@ class _SignupPageState extends State<SignupPage> {
                           : 'assets/tableau.png',
                       scale: 1.5,
                     ),
-                    SizedBox(width: screenWidth * .3),
-                  ],
-                ),
-                SizedBox(height: screenHeight * .05),
-                Center(
-                  child: Text(
-                    "Create an account",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 79, 41, 159),
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-                SizedBox(height: screenHeight * .02),
-
-                myTextField(
-                  labelText: 'Name',
-                  icon: Icons.person,
-                  controller: fullNameController,
-                ),
-                SizedBox(height: screenHeight * .01),
-
-                myTextField(
-                  labelText: 'Email',
-                  icon: Icons.email,
-                  controller: emailController,
-                ),
-                SizedBox(height: screenHeight * .01),
-
-                myTextField(
-                  labelText: 'Phone',
-                  icon: Icons.phone,
-                  controller: phoneController,
-                ),
-                SizedBox(height: screenHeight * .01),
-
-                myTextField(
-                  labelText: 'Password',
-                  icon: Icons.password,
-                  controller: passwordController,
-                ),
-                SizedBox(height: screenHeight * .01),
-
-                myTextField(
-                  labelText: 'Confirm Password',
-                  icon: Icons.password,
-                  controller: confirmPasswordController,
-                ),
-                SizedBox(height: screenHeight * .02),
-
-                // Gender Toggle
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Gender:",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 79, 41, 159),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      ChoiceChip(
-                        label: Text(
-                          "Male",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        selected: gender == "Male",
-                        onSelected: (_) {
-                          setState(() {
-                            gender = "Male";
-                          });
-                        },
-                        selectedColor: Colors.deepPurple,
-                        labelStyle: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(width: 10),
-                      ChoiceChip(
-                        label: Text("Female"),
-                        selected: gender == "Female",
-                        onSelected: (_) {
-                          setState(() {
-                            gender = "Female";
-                          });
-                        },
-                        selectedColor: Colors.purpleAccent,
-                        labelStyle: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: screenHeight * .015),
-
-                // Role Toggle
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Role:",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 79, 41, 159),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      ChoiceChip(
-                        label: Text("Teacher"),
-                        selected: role == "teacher",
-                        onSelected: (_) {
-                          setState(() {
-                            role = "teacher";
-                          });
-                        },
-                        selectedColor: Colors.deepPurple,
-                        labelStyle: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(width: 10),
-                      ChoiceChip(
-                        label: Text("Parent"),
-                        selected: role == "parent",
-                        onSelected: (_) {
-                          setState(() {
-                            role = "parent";
-                          });
-                        },
-                        selectedColor: Colors.purpleAccent,
-                        labelStyle: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: screenHeight * .05),
-
-                isLoading
-                    ? CircularProgressIndicator()
-                    : Padding(
-                        padding: const EdgeInsets.fromLTRB(140, 0, 140, 0),
-                        child: myOutlinedButton(
-                          text: "Submit",
-                          onTap: () async {
-                            setState(() => isLoading = true);
-
-                            await apiService.createUser(
-                              fullNameController.text,
-                              emailController.text,
-                              passwordController.text,
-                              confirmPasswordController.text,
-                              role,
-                              phoneController.text,
-                              gender,
-                            );
-
-                            setState(() => isLoading = false);
-                          },
-                        ),
-                      ),
-                SizedBox(height: screenHeight * .02),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Already have an account? ",
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Create an account",
                       style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
+                        color: Color.fromARGB(255, 207, 207, 207),
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginPage(),
+                    const SizedBox(height: 20),
+
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          myTextField(
+                            labelText: 'Name',
+                            icon: Icons.person,
+                            controller: fullNameController,
                           ),
-                        );
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.purple,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
+                          const SizedBox(height: 10),
+                          myTextField(
+                            labelText: 'Email',
+                            icon: Icons.email,
+                            controller: emailController,
+                          ),
+                          const SizedBox(height: 10),
+                          myTextField(
+                            labelText: 'Phone',
+                            icon: Icons.phone,
+                            controller: phoneController,
+                          ),
+                          const SizedBox(height: 10),
+                          myTextField(
+                            isPassword: true,
+                            labelText: 'Password',
+                            icon: Icons.lock,
+                            controller: passwordController,
+                          ),
+                          const SizedBox(height: 10),
+                          myTextField(
+                            isPassword: true,
+                            labelText: 'Confirm Password',
+                            icon: Icons.lock_outline,
+                            controller: confirmPasswordController,
+                          ),
+                        ],
                       ),
                     ),
+
+                    const SizedBox(height: 15),
+
+                    // Gender Selection
+                    buildToggleRow(
+                      title: "Gender",
+                      options: ["Male", "Female"],
+                      selected: gender,
+                      onSelected: (val) => setState(() => gender = val),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    // Role Selection
+                    buildToggleRow(
+                      title: "Role",
+                      options: ["teacher", "parent"],
+                      selected: role,
+                      onSelected: (val) => setState(() => role = val),
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    isLoading
+                        ? const CircularProgressIndicator()
+                        : myOutlinedButton(
+                            text: "Submit",
+                            onTap: () async {
+                              setState(() => isLoading = true);
+
+                              await apiService.createUser(
+                                fullNameController.text,
+                                emailController.text,
+                                passwordController.text,
+                                confirmPasswordController.text,
+                                role,
+                                phoneController.text,
+                                gender,
+                              );
+
+                              setState(() => isLoading = false);
+                            },
+                          ),
+
+                    const SizedBox(height: 20),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Already have an account? ",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 209, 209, 209),
+                              fontSize: 16),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginPage()),
+                            );
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
                   ],
                 ),
-              ],
+              ),
             ),
-          ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget buildToggleRow({
+    required String title,
+    required List<String> options,
+    required String selected,
+    required Function(String) onSelected,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          Text(
+            "$title:",
+            style: const TextStyle(
+              color: Color.fromARGB(255, 79, 41, 159),
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(width: 15),
+          ...options.map((option) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: ChoiceChip(
+                label: Text(option[0].toUpperCase() + option.substring(1)),
+                selected: selected == option,
+                onSelected: (_) => onSelected(option),
+                selectedColor: Colors.deepPurple,
+                backgroundColor: Colors.blueGrey,
+                labelStyle: const TextStyle(color: Colors.white),
+              ),
+            );
+          }).toList(),
+        ],
       ),
     );
   }
