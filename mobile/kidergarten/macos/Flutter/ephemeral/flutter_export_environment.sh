9611171c338c05e5c0836b5ad0d46ca29575b8e1
Ilyes-Mekhalfa <ilyes.mekalfa@gmail.com>
2025-04-15 13:19:32 +0200

    ee

--- mobile/kidergarten/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/kidergarten/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutterSDK\flutter"
+export "FLUTTER_ROOT=D:\flutter\flutter\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\PC\Desktop\khliha3laAllah\mobile\kidergarten"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
