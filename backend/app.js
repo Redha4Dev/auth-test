bb0c54ccf570b5a5847fb3099d313e6c0a2b3bf3
Ilyes-Mekhalfa <ilyes.mekalfa@gmail.com>
2025-04-16 10:22:29 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -10,38 +10,39 @@ const  teacher  = require('./Routes/teacherroute');
 //creating the app
 const app = express();
 //security
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const helmet = require('helmet');
 // const expresssslify = require('express-sslify');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-// // const csurf = require('csurf');
 
-// // app.use(csurf({ cookie: true })); //CSRF protection
 app.use(helmet()); //http headers security
-// app.use(expresssslify.HTTPS({ trustProtoHeader: true })); //force HTTPS
+app.set('trust proxy', 1)
+// app.use(expresssslify.HTTPS()); //force HTTPS
 app.use(cookieParser());
 app.use(mongoSanitize());//sanitize from mongo injection
 app.use(xss()); //sanitize from xss injections
 app.use(hpp()); //prevent http from parameter pollution
-// const limiter = rateLimit({
-//     max: 100,
-//     windowMs: 60 * 60 * 1000,
-//     message: 'Too many requests from this IP, please try again in an hour!'
-// });//limit the number of request from a IP adress to 100 requests in hour to protect the server from teh DDos attaks
-// app.use('/api', limiter);
+const limiter = rateLimit({
+    limit: 100, //limit each IP to 100 requests per windowMs
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
+    legacyHeaders: false, //disable the legacy headers
+     standardHeaders: true //enable the standard headers 
+});//limit the number of request from a IP adress to 100 requests in hour to protect the server from teh DDos attaks
+app.use('/api', limiter);
 
 
 
 
 
 
-// if (process.env.NODE_ENV === 'development') {
-//     app.use (morgan('dev'))
-// }
+if (process.env.NODE_ENV === 'development') {
+    app.use (morgan('dev'))
+}
 
 
 app.use(cors());
