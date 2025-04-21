37b0e0466174139e8ee2b92032457033bd0f40db
Zakaria Medjdoub <mz.medjdoub@esi-sba.dz>
2025-04-21 18:27:40 +0100

    Signup Page clean up

--- mobile/lib/pages/ParentProfile.dart
+++ mobile/lib/pages/ParentProfile.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:kidergarten/components/studentCard.dart';
 
-
-
 class HomePage extends StatelessWidget {
   final Color primaryColor = Color(0xFF5F3DC4);
-  final TextStyle headerStyle = TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
-  final TextStyle tableHeaderStyle = TextStyle(color: Colors.white, fontSize: 16);
+  final TextStyle headerStyle =
+      TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
+  final TextStyle tableHeaderStyle =
+      TextStyle(color: Colors.white, fontSize: 16);
   final TextStyle tableCellStyle = TextStyle(color: Colors.black, fontSize: 14);
 
   @override
@@ -16,16 +16,16 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: SizedBox(
-       height: 59, 
-       child: BottomNavigationBar(
-         iconSize: 30,  
-         elevation: 8.0,
-         backgroundColor: primaryColor,
-         selectedItemColor: Colors.white,
-         unselectedItemColor: Colors.white54,
-         items: const [
-           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-           BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
+        height: 59,
+        child: BottomNavigationBar(
+          iconSize: 30,
+          elevation: 8.0,
+          backgroundColor: primaryColor,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
           ],
         ),
       ),
@@ -34,24 +34,44 @@ class HomePage extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                height: screenHeight*0.155,
+                height: screenHeight * 0.155,
                 color: primaryColor,
                 child: Center(
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.grey.shade300,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                          radius: 40, backgroundColor: Colors.grey[300]),
+                      
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Name: ", style: TextStyle(color: Colors.white)),
+                          Text("Email: ",
+                              style: TextStyle(color: Colors.white)),
+                          Text("Phone Number ",
+                              style: TextStyle(color: Colors.white)),
+                        ],
+                        //+ globalParentData?['name']
+                      ),
+                    ],
                   ),
                 ),
               ),
-             Container(
+              Container(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
                 color: Color(0xFFD9C7F3),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: Text("Weekly Menu", style: headerStyle.copyWith(color: const Color.fromARGB(255, 255, 255, 255), fontWeight: FontWeight.bold)),
+                  child: Text("Weekly Menu",
+                      style: headerStyle.copyWith(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          fontWeight: FontWeight.bold)),
                 ),
               ),
-
               Table(
                 border: TableBorder.all(color: Colors.black),
                 columnWidths: {
@@ -63,12 +83,21 @@ class HomePage extends StatelessWidget {
                   TableRow(
                     decoration: BoxDecoration(color: primaryColor),
                     children: [
-                      Padding(padding: EdgeInsets.all(8), child: Text("", style: tableHeaderStyle)),
-                      Padding(padding: EdgeInsets.all(8), child: Text("Snacks", style: tableHeaderStyle)),
-                      Padding(padding: EdgeInsets.all(8), child: Text("Lunch", style: tableHeaderStyle)),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text("", style: tableHeaderStyle)),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text("Snacks", style: tableHeaderStyle)),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text("Lunch", style: tableHeaderStyle)),
                     ],
                   ),
-                  ...["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"].asMap().entries.map((entry) {
+                  ...["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+                      .asMap()
+                      .entries
+                      .map((entry) {
                     final index = entry.key;
                     final day = entry.value;
                     final snacks = [
@@ -85,33 +114,37 @@ class HomePage extends StatelessWidget {
                       "Pasta with meat\nYogurt",
                       "Mashed potato + Meat"
                     ];
-                    
+
                     return TableRow(children: [
-                      Padding(padding: EdgeInsets.all(8), child: Text(day, style: tableCellStyle)),
-                      Padding(padding: EdgeInsets.all(8), child: Text(snacks[index], style: tableCellStyle)),
-                      Padding(padding: EdgeInsets.all(8), child: Text(lunch[index], style: tableCellStyle)),
-                      
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(day, style: tableCellStyle)),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(snacks[index], style: tableCellStyle)),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(lunch[index], style: tableCellStyle)),
                     ]);
                   }).toList()
                 ],
               ),
-             Container(
-               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-               color: Color(0xFFD9C7F3),
-               child: Align(
-                 alignment: Alignment.centerLeft,
-                 child: Text(
-                   "List Of Children",
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                color: Color(0xFFD9C7F3),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "List Of Children",
                     style: headerStyle.copyWith(
                       color: Color.fromARGB(255, 255, 255, 255),
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-             ),
-
-             StudentCard(name: "tfdct", age: 4, gender: "Mouaad"),
-             StudentCard(name: "Anis Boualdja", age: 5, gender: "Male"),
+              ),
+              StudentCard(name: "tfdct", age: 4, gender: "Mouaad"),
+              StudentCard(name: "Anis Boualdja", age: 5, gender: "Male"),
             ],
           ),
         ),
@@ -125,15 +158,19 @@ class ChildCard extends StatelessWidget {
   final int age;
   final String childClass;
 
-  const ChildCard({required this.name, required this.age, required this.childClass});
+  const ChildCard(
+      {required this.name, required this.age, required this.childClass});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16), side: BorderSide(color: Colors.purple.shade200)),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(color: Colors.purple.shade200)),
       child: ListTile(
-        leading: CircleAvatar(backgroundImage: AssetImage('assets/avatar_placeholder.png')),
+        leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/avatar_placeholder.png')),
         title: Text("Full Name: $name"),
         subtitle: Text("Age: $age\nClass: $childClass"),
         trailing: Icon(Icons.more_vert),
