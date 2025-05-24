fb19055639250a6bb26ea78adaa042d95782e26b
Redha4Dev <devredha@gmail.com>
2025-05-25 00:38:48 +0200

    editing

--- backend/Routes/mealroute.js
+++ backend/Routes/mealroute.js
@@ -5,6 +5,8 @@ const router = express.Router();
 router
     .route('/:id')
     .get(mealController.getMeal)
+router
+    .route('/')
     .post(mealController.createMeal)
 
 module.exports = router;
