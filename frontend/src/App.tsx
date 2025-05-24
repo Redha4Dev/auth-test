94709ba6c8e0a563053dc1439bb57e6638bc36e5
MouadAllouche <160075882+MouadAllouche@users.noreply.github.com>
2025-05-24 22:31:58 +0200

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,12 +21,9 @@ import Inbox from "./Pages/Inbox";
 import Mail from "./Pages/Mail";
 import Settings from "./Pages/Settings";
 import Teachers from "./Pages/Users/Teachers";
-<<<<<<< HEAD
 import EditKidPage from "./Pages/editKidPage";
 import EmailVerification from "./Pages/emailVerif";
-=======
 import TeacherProfile from "./Pages/Users/TeacherProfile";
->>>>>>> dd8f0c89cba939ae61c691af502ef7140a91829e
 
 function App() {
 
