25261040892afcbd4fcfc126fcd4694d43f2b8fe
MouadAllouche <160075882+MouadAllouche@users.noreply.github.com>
2025-05-25 04:17:12 +0200

    ...

--- frontend/src/Services/authService.js
+++ frontend/src/Services/authService.js
@@ -78,7 +78,7 @@ export const updateKidInfos = async (kidData) => {
 // Update Parent Information
 export const updateParentInfos = async (parentData) => {
   try {
-    const response = await axios.patch(`${API_URL}/teacher`, parentData, {
+    const response = await axios.patch(`${API_URL}/parent`, parentData, {
       withCredentials: true,
     });
     console.log(response);
@@ -92,7 +92,7 @@ export const updateParentInfos = async (parentData) => {
 // Update Teacher Information  
 export const updateTeacherInfos = async (teacherData) => {
   try {
-    const response = await axios.patch(`${API_URL}/parent`, teacherData, {
+    const response = await axios.patch(`${API_URL}/teacher`, teacherData, {
       withCredentials: true,
     });
     console.log(response);
